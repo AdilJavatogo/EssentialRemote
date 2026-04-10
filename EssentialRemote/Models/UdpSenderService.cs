fa74a034ce84749810596cca50a926edf655f116
AdilJavatogo <Adil2300@hotmail.com>
2026-04-10 13:01:04 +0200

    port localhost

--- EssentialRemote/Models/UdpSenderService.cs
+++ EssentialRemote/Models/UdpSenderService.cs
@@ -8,7 +8,7 @@ namespace EssentialRemote.Models
     public class UdpSenderService
     {
         private readonly UdpClient _udpClient;
-        private readonly string _targetIpAddress = "172.31.47.53"; // PC ip -adresse, hvor Python-scriptet kører, lige pt. hente det påny med hostname -I kommandoen i terminalen på PC'en 
+        private readonly string _targetIpAddress = "127.0.0.1"; // PC ip -adresse, hvor Python-scriptet kører, lige pt. hente det påny med hostname -I kommandoen i terminalen på PC'en, emulator: 10.0.2.2
         private readonly int _targetPort = 5000; // Porten som Python scriptet skal lytte på
 
         public UdpSenderService()
