5d8bdb02f8137cc657f0a279233e881cfec7eb75
AdilJavatogo <Adil2300@hotmail.com>
2026-04-10 09:44:44 +0200

    ......

--- EssentialRemote/Models/TwistMessage.cs
+++ EssentialRemote/Models/TwistMessage.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace EssentialRemote.Models
 {
     public class TwistMessage
     {
+        [JsonPropertyName("linearX")]
         public float LinearX { get; set; }
+
+        [JsonPropertyName("angularZ")]
         public float AngularZ { get; set; }
 
         // En lille hjælpemetode til at lave objektet om til JSON
